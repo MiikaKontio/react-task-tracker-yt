94721556b2f49404bfdbd5a257a71c3c01a52a30
Miika Kontio <miika.kontio@prime.fi>
2023-09-05 22:11:33 +0300

    yet better UI

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -10,7 +10,7 @@ import { useState } from 'react';
 import { getCurrentDateAsJson } from '../../utils/DateTimeUtils';
 import { updateToFirebaseById } from '../../datatier/datatier';
 import AddMovie from './AddMovie';
-import { Button, Card } from 'react-bootstrap';
+import { Button, ButtonGroup, Card } from 'react-bootstrap';
 
 export default function Movie({ movie, onDelete, onEdit }) {
 
@@ -39,23 +39,28 @@ export default function Movie({ movie, onDelete, onEdit }) {
     return (
         <>
 
-            <Card style={{ marginBottom: '10px' }}>
-                <Card.Header as="h5">{movie.name} {movie.publishYear > 0 ? '(' + movie.publishYear + ')' : ''}
-
+            <Card className='cardCustom' style={{ marginBottom: '10px' }}>
+                <Card.Header>
                     <RightWrapper>
-                        <Icon name={Constants.ICON_EDIT} className={'btn ' + Constants.CLASSNAME_EDITBTN}
-                            style={{ color: 'light-gray', cursor: 'pointer', fontSize: '1.2em' }}
-                            onClick={() => editable ? setEditable(false) : setEditable(true)} />
-                        <Icon className={'btn ' + Constants.CLASSNAME_DELETEBTN}
-                            name={Constants.ICON_DELETE}
-                            color={Constants.COLOR_DELETEBUTTON}
-                            style={{ cursor: 'pointer', fontSize: '1.2em' }}
-                            onClick={() => {
-                                if (window.confirm(t('delete_movie_confirm_message'))) {
-                                    onDelete(movie.id);
-                                }
-                            }} />
+                        <ButtonGroup>
+                            <Icon name={Constants.ICON_EDIT} className={'btn ' + Constants.CLASSNAME_EDITBTN}
+                                style={{ color: 'light-gray', cursor: 'pointer', fontSize: '1.2em' }}
+                                onClick={() => editable ? setEditable(false) : setEditable(true)} />
+                            <Icon className={'btn ' + Constants.CLASSNAME_DELETEBTN}
+                                name={Constants.ICON_DELETE}
+                                color={Constants.COLOR_DELETEBUTTON}
+                                style={{ cursor: 'pointer', fontSize: '1.2em' }}
+                                onClick={() => {
+                                    if (window.confirm(t('delete_movie_confirm_message'))) {
+                                        onDelete(movie.id);
+                                    }
+                                }} />
+                        </ButtonGroup>
                     </RightWrapper>
+                    {movie.name} {movie.publishYear > 0 ? '(' + movie.publishYear + ')' : ''}
+                    <div style={{ fontWeight: 'normal' }}>
+                        <StarRating starCount={movie.stars} />
+                    </div>
                 </Card.Header>
                 <Card.Body>
                     <Card.Subtitle>{movie.nameFi !== "" ? movie.nameFi : ''}</Card.Subtitle>
@@ -65,7 +70,7 @@ export default function Movie({ movie, onDelete, onEdit }) {
                         }</span>) : ('')}
                         <br />
                         {movie.description}
-                        <StarRating starCount={movie.stars} />
+
                         {
                             editable && <AddMovie
                                 movieID={movie.id}
